bded7e6dae1ed957a463aa8ca2936dde0baf82aa
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-04-18 21:56:23 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -1,19 +1,16 @@
+// Copyright 2024 alenkaLo
 #include "textgen.h"
-
 std::map<prefix, std::vector<std::string>>
-ReadFile(int NPREF, std::string path) {
+ReadFile(int NPREF, std::string path){
     prefix pref;
     std::map<prefix, std::vector<std::string>> statetab; // префикс-суффиксы
     std::string word;
     std::ifstream in(path);
-    if (in.is_open())
-    {
-        while (in >> word)
-        {
-            if (pref.size() < NPREF) {
+    if (in.is_open()){
+        while (in >> word){
+            if (pref.size() < NPREF){
                 pref.push_back(word);
-            }
-            else {
+            }else{
                 statetab[pref].push_back(word);
                 pref.push_back(word);
                 pref.pop_front();
@@ -24,12 +21,12 @@ ReadFile(int NPREF, std::string path) {
     return statetab;
 }
 
-std::string genWord(std::deque<std::string>(* start), std::map<std::deque<std::string>, std::vector<std::string>> statetab, std::string& text) {
+std::string genWord(std::deque<std::string>(* start), std::map<std::deque<std::string>, std::vector<std::string>> statetab, std::string(* text)){
     std::vector<std::string> variable = statetab[*start];
     int index = 0;
-    if (variable.size() > 0) {
+    if (variable.size() > 0){
         index = rand() % variable.size();
-        text = text + variable[index] + " ";
+        *text = *text + variable[index] + " ";
         (*start).pop_front();
         (*start).push_back(variable[index]);        
     } 
@@ -39,11 +36,10 @@ std::string genWord(std::deque<std::string>(* start), std::map<std::deque<std::s
 std::string generateText(int NPREF, int MAXGEN,
     std::deque<std::string> start,
     std::map<std::deque<std::string>,
-    std::vector<std::string>> statetab) {
+    std::vector<std::string>> statetab){
     std::string text;
-    for (int i = 0; i < MAXGEN; i++)
-    {
-        genWord(&start, statetab, text);
+    for (int i = 0; i < MAXGEN; i++){
+        genWord(&start, statetab, &text);
     }
     return text;
 }
