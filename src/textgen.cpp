68e1e64ce6413c2765625ad665990a365bdd7335
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-04-18 21:59:19 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -1,16 +1,16 @@
 // Copyright 2024 alenkaLo
 #include "textgen.h"
 std::map<prefix, std::vector<std::string>>
-ReadFile(int NPREF, std::string path){
+ReadFile(int NPREF, std::string path) {
     prefix pref;
     std::map<prefix, std::vector<std::string>> statetab; // префикс-суффиксы
     std::string word;
     std::ifstream in(path);
-    if (in.is_open()){
+    if (in.is_open()) {
         while (in >> word){
-            if (pref.size() < NPREF){
+            if (pref.size() < NPREF) {
                 pref.push_back(word);
-            }else{
+            }else {
                 statetab[pref].push_back(word);
                 pref.push_back(word);
                 pref.pop_front();
@@ -21,24 +21,25 @@ ReadFile(int NPREF, std::string path){
     return statetab;
 }
 
-std::string genWord(std::deque<std::string>(* start), std::map<std::deque<std::string>, std::vector<std::string>> statetab, std::string(* text)){
+std::string genWord(std::deque<std::string>(* start), std::map<std::deque<std::string>,
+std::vector<std::string>> statetab, std::string(* text)){
     std::vector<std::string> variable = statetab[*start];
     int index = 0;
-    if (variable.size() > 0){
-        index = rand() % variable.size();
+    if (variable.size() > 0) {
+        index = rand_r() % variable.size();
         *text = *text + variable[index] + " ";
         (*start).pop_front();
-        (*start).push_back(variable[index]);        
-    } 
+        (*start).push_back(variable[index]);
+    }
     return variable[index];
 }
 
 std::string generateText(int NPREF, int MAXGEN,
     std::deque<std::string> start,
     std::map<std::deque<std::string>,
-    std::vector<std::string>> statetab){
+    std::vector<std::string>> statetab) {
     std::string text;
-    for (int i = 0; i < MAXGEN; i++){
+    for (int i = 0; i < MAXGEN; i++) {
         genWord(&start, statetab, &text);
     }
     return text;
