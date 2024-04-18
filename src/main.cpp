c779e3b5a35e93dece26e02310cbb60cb01bc976
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-04-18 22:28:41 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
+// Copyright 2024 alenkaLo
 #include <iostream>
 #include "textgen.h"
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Rus");
     const int NPREF = 2; // количество слов в префиксе
     const int MAXGEN = 700; //объем текста на выходе
     std::string path = "InputText.txt";
     std::map<prefix, std::vector<std::string>> statetab;
-    statetab=ReadFile(NPREF, path);
+    statetab = ReadFile(NPREF, path);
     prefix start = { "Умная", "собачка" };
-    
-    std::cout << generateText(NPREF,MAXGEN,start,statetab);
+    std::cout << generateText(NPREF, MAXGEN, start, statetab);
 }
