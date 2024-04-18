af2a6673fc0ecf9af0bcb1c2bba7c3cc3e3b294a
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-04-18 22:44:38 +0300

    Create tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,12 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2024 alenkaLo
 #include <gtest/gtest.h>
+#include "..\6.Markov\textgen.h"
+TEST(TestCaseName, TestName2) {
+    std::string path = "InputText.txt";
+    std::map<prefix, std::vector<std::string>> statetab;
+    statetab = ReadFile(2, path);
+    prefix pref;
+    pref = statetab.begin()->first;
+    int result = pref.size();
+    EXPECT_EQ(result, 2);
+}
