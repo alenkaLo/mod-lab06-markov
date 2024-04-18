68b6e0cf75ee33b0d1d88e03b635cc4f36f4b230
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-04-18 19:23:06 +0300

    Create textgen.cpp

---
+++ src/textgen.cpp
@@ -0,0 +1,50 @@
+#include "textgen.h"
+
+std::map<prefix, std::vector<std::string>>
+ReadFile(int NPREF, std::string path) {
+    prefix pref;
+    std::map<prefix, std::vector<std::string>> statetab; // префикс-суффиксы
+    std::string word;
+    std::ifstream in(path);
+    if (in.is_open())
+    {
+        while (in >> word)
+        {
+            if (pref.size() < NPREF) {
+                pref.push_back(word);
+            }
+            else {
+                statetab[pref].push_back(word);
+                pref.push_back(word);
+                pref.pop_front();
+            }
+        }
+    }
+    in.close();     // закрываем файл
+    return statetab;
+}
+
+std::string genWord(std::deque<std::string>(* start), std::map<std::deque<std::string>, std::vector<std::string>> statetab, std::string& text) {
+    std::vector<std::string> variable = statetab[*start];
+    int index = 0;
+    if (variable.size() > 0) {
+        index = rand() % variable.size();
+        text = text + variable[index] + " ";
+        (*start).pop_front();
+        (*start).push_back(variable[index]);        
+    } 
+    return variable[index];
+}
+
+std::string generateText(int NPREF, int MAXGEN,
+    std::deque<std::string> start,
+    std::map<std::deque<std::string>,
+    std::vector<std::string>> statetab) {
+    std::string text;
+    for (int i = 0; i < MAXGEN; i++)
+    {
+        genWord(&start, statetab, text);
+    }
+    return text;
+}
+
